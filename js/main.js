212e343bcdacd440972e3f01d87c5e87602c736b
trainerpemrograman <trainer_pemrograman@gamelab.id>
2023-09-27 16:04:32 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 (function ($) {
   "use strict";
 
-  //   // Spinner
-  //   var spinner = function () {
-  //     setTimeout(function () {
-  //       if ($("#spinner").length > 0) {
-  //         $("#spinner").removeClass("show");
-  //       }
-  //     }, 1);
-  //   };
-  //   spinner();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
   // Initiate the wowjs
   new WOW().init();
